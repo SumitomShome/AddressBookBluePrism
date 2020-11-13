b9683dae5b08eff8e99f53d55f477d11fbc16383
SumitomShome <sumitomshome@gmail.com>
2020-11-13 22:32:50 +0530

    [Sumitom] Add: Method to create table in database

---
+++ UC1-CreationOfTable.sql
@@ -0,0 +1,12 @@
+USE addressbook_db;
+CREATE TABLE addressbook_table
+(
+first_name varchar(25) NOT NULL,
+last_name varchar(25) NOT NULL,
+address varchar(150) NOT NULL,
+city varchar(25) NOT NULL,
+state varchar(25) NOT NULL,
+zip int NOT NULL,
+phone_number int NOT NULL,
+email varchar(100) NOT NULL
+);
